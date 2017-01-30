bbaef06b9c832e70af9b3c89adf4ae4591448d9b
Amir Bawab <amirbawab@gmail.com>
2017-01-30 14:01:18 -0500

    Added first boost log message

--- src/EasyCC.cpp
+++ src/EasyCC.cpp
@@ -4,10 +4,47 @@
 #include <vector>
 #include <string>
 
+#include <boost/move/utility_core.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace keywords = boost::log::keywords;
+namespace sinks = boost::log::sinks;
+
 using namespace std;
 using namespace ecc;
 
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(ecc_logger, src::logger_mt)
+
+void init_log() {
+    logging::add_console_log(
+            std::cout,
+            boost::log::keywords::format = "[%TimeStamp%]: %Message%"
+    );
+    logging::add_file_log(
+            keywords::file_name = "sample_%N.log",
+            keywords::rotation_size = 10 * 1024 * 1024,                                   /* rotate files every 10 MiB... */
+            keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0), /* ...or at midnight            */
+            keywords::auto_flush = true,
+            keywords::format = "[%TimeStamp%]: %Message%"
+    );
+    logging::add_common_attributes();
+}
+
 int main() {
+
+    // Configure the logger
+    init_log();
+
+    // Test logging system
+    src::logger_mt& lg = ecc_logger::get();
+    BOOST_LOG(lg) << "Hello world";
 
     // Lexical analysis phase
     Lexical lexical(
