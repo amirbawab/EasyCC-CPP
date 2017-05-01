44b383e39b1dda8b5f478cf643d0feaa8158bc34
Amir Bawab <amirbawab@gmail.com>
2017-05-01 16:55:59 -0400

    Added semantic action handlers for calculator

--- examples/calculator/calculator.cpp
+++ examples/calculator/calculator.cpp
@@ -1,6 +1,11 @@
 #include "easycc/EasyCC.h"
 #include <iostream>
 
+#include <stack>
+#include <fstream>
+
+typedef std::vector<std::shared_ptr<ecc::LexicalToken>> Tokens;
+
 int main(int argc, char *argv[]) {
     ecc::EasyCC easyCC;
 
@@ -12,14 +17,62 @@ int main(int argc, char *argv[]) {
         return code;
     }
 
-    // Get file name
-    std::string outputFileName = easyCC.getOutputFileName();
+    // Create file
+    std::ofstream outputFile(easyCC.getOutputFileName());
+
+    // Check if can open file
+    if(!outputFile.is_open()) {
+        std::cerr << "Cannot open file!" << std::endl;
+        return 1;
+    }
+
+    // Create stack for computing the expression
+    std::stack<int> calc;
 
     // Register events
-    easyCC.registerSemanticAction("#className#",[&](int phase,
-                                         std::vector<std::shared_ptr<ecc::LexicalToken>> &lexicalVector,
-                                         int index, bool stable){
-        std::cout << "Class name " << lexicalVector[index]->getValue() << std::endl;
+    easyCC.registerSemanticAction("#push#",[&](int phase, Tokens &lexicalVector, int index, bool stable){
+        calc.push(std::stoi(lexicalVector[index]->getValue()));
+    });
+
+    easyCC.registerSemanticAction("#plus#",[&](int phase, Tokens &lexicalVector, int index, bool stable){
+        int popR = calc.top();
+        calc.pop();
+        int popL = calc.top();
+        calc.pop();
+        calc.push(popL + popR);
+    });
+
+    easyCC.registerSemanticAction("#minus#",[&](int phase, Tokens &lexicalVector, int index, bool stable){
+        int popR = calc.top();
+        calc.pop();
+        int popL = calc.top();
+        calc.pop();
+        calc.push(popL - popR);
+    });
+
+    easyCC.registerSemanticAction("#multiply#",[&](int phase, Tokens &lexicalVector, int index, bool stable){
+        int popR = calc.top();
+        calc.pop();
+        int popL = calc.top();
+        calc.pop();
+        calc.push(popL * popR);
+    });
+
+    easyCC.registerSemanticAction("#divide#",[&](int phase, Tokens &lexicalVector, int index, bool stable){
+        int popR = calc.top();
+        calc.pop();
+        int popL = calc.top();
+        calc.pop();
+        calc.push(popL / popR);
+    });
+
+    easyCC.registerSemanticAction("#print#",[&](int phase, Tokens &lexicalVector, int index, bool stable){
+        outputFile << "Expression result: " << calc.top() << std::endl;
+        calc.pop();
+    });
+
+    easyCC.registerSemanticAction("#end#",[&](int phase, Tokens &lexicalVector, int index, bool stable){
+        outputFile.close();
     });
 
     // Start compiling
