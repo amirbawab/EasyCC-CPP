ba5ca2b1230ed691bf2f34d4b540e2d2f8f44bef
Amir Bawab <amirbawab@gmail.com>
2017-05-02 16:15:54 -0400

    Added get vector of all non terminals

--- include/easycc/Grammar.h
+++ include/easycc/Grammar.h
@@ -59,8 +59,13 @@ namespace ecc {
          */
         static std::string extractTerminal(std::string terminal);
 
+        /**
+         * Get vector of non terminals
+         * @return vector
+         */
+        std::vector<std::string> getNonTerminals() const;
+
     private:
-        friend class ConvertFF;
         std::string m_start;
         std::map<std::string, std::shared_ptr<std::vector<std::shared_ptr<std::vector<std::string>>>>> m_productions;
         std::map<std::shared_ptr<std::vector<std::string>>, std::shared_ptr<std::set<std::string>>> m_productionFirstSet;
