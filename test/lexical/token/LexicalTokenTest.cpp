66b745b99a6e09c065ac8726f2972cd24d447cc6
Amir Bawab <amirbawab@gmail.com>
2017-05-06 17:28:03 -0400

    Updated gtest

--- test/lexical/token/LexicalTokenTest.cpp
+++ test/lexical/token/LexicalTokenTest.cpp
@@ -2,11 +2,10 @@
 #include <gtest/gtest.h>
 
 TEST(LexicalTokenTest, constructorValues) {
-    ecc::LexicalToken lexicalTokenNormal(ecc::LexicalToken::Type::NORMAL_TOKEN,"name", "value", 1, 2, 3);
-    ecc::LexicalToken lexicalTokenError(ecc::LexicalToken::Type::ERROR_TOKEN,"name", "value", 1, 2, 3);
+    ecc::LexicalToken lexicalTokenNormal(ecc::LexicalToken::Type::NORMAL_TOKEN,"name", "value", 1, 2);
+    ecc::LexicalToken lexicalTokenError(ecc::LexicalToken::Type::ERROR_TOKEN,"name", "value", 1, 2);
     EXPECT_EQ(lexicalTokenNormal.getLine(),1);
     EXPECT_EQ(lexicalTokenNormal.getColumn(),2);
-    EXPECT_EQ(lexicalTokenNormal.getPosition(),3);
     EXPECT_EQ(lexicalTokenNormal.getName(),"name");
     EXPECT_EQ(lexicalTokenNormal.getValue(),"value");
     EXPECT_EQ(lexicalTokenNormal.getType(), ecc::LexicalToken::Type::NORMAL_TOKEN);
