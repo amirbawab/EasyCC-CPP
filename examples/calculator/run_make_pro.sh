b16de003c88a0f880dc1b141148a79d7d846809d
Amir Bawab <amirbawab@gmail.com>
2017-07-07 19:52:02 -0400

    Adjusted script comment

--- examples/calculator/run_make_pro.sh
+++ examples/calculator/run_make_pro.sh
@@ -3,10 +3,12 @@
 # Generate header file
 make generate_files
 
-# NOTE: The following 2 steps might not be required in other project.
+# NOTE: The following step might not be required in other project.
 # However, for this program it is needed because the example is
 # a subfolder of EasyCC-CPP
 cp -ar ../../bin/include/ EasyCC-CPP/bin/
+
+# Refresh c++ header files to include the new generated ones
 ./run_cmake.sh
 
 # Build project
