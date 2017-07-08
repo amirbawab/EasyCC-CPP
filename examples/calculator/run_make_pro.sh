7c8b2f29715db4b25bf21d06682e6056876e8005
Amir Bawab <amirbawab@gmail.com>
2017-07-08 12:39:54 -0400

    Removed unused command

--- examples/calculator/run_make_pro.sh
+++ examples/calculator/run_make_pro.sh
@@ -3,11 +3,6 @@
 # Generate header file
 make generate_files
 
-# NOTE: The following step might not be required in other project.
-# However, for this program it is needed because the example is
-# a subfolder of EasyCC-CPP
-cp -ar ../../bin/include/ EasyCC-CPP/bin/
-
 # Refresh c++ header files to include the new generated ones
 ./run_cmake.sh
 
